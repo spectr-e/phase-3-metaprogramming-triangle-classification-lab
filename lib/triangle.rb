c3651089f35aaf5e5969ed891fff4385fa2d66dd
spectr-e <devs.josia@gmail.com>
2023-02-14 10:57:01 +0300

    completed assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,48 @@
 class Triangle
   # write code here
+attr_reader :a, :b, :c
+
+def initialize(a, b, c)
+  @a = a
+  @b = b
+  @c = c
+end
+
+def kind
+  validate_triangle
+  if a == b && b == c
+    :equilateral
+  elsif a == b || b == c || a == c
+    :isosceles
+  else
+    :scalene
+  end
+end
+
+def side_greater_zero
+  if a > 0 && b > 0 && c > 0
+    true
+  else
+    false
+  end
+end
+
+def triangle_inequality
+  sumA = [a, b].sum
+  sumB = [b, c].sum
+  sumC = [c, a].sum
+
+  if sumA > c && sumB > a && sumC > b
+    true
+  else
+    false
+  end
+end
+
+def validate_triangle
+  raise TriangleError unless triangle_inequality == true && side_greater_zero == true
+end
+
+class TriangleError < StandardError
+end
 end
